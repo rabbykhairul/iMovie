aa1fc0d84d4db46d0f18fca99b63fd35ab3d2a5f
rabbykhairul <rabbykhairul@gmail.com>
2021-10-04 18:23:23 +0600

    render all the grouped menues inside 'LeftSideBar'

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+
 import Header from "./commons/Header";
+import ListNavigation from "./commons/ListNavigation";
+
+import { MAIN_MENU, LIBRARY, CATEGORY, GENERAL } from "../utils/menuItems";
 
 const LeftSideBar = () => {
   return (
     <nav className="left-side-bar">
       <Header />
+      <div className="grouped-menu-container">
+        <ListNavigation title={MAIN_MENU.title} items={MAIN_MENU.items} />
+        <ListNavigation title={LIBRARY.title} items={LIBRARY.items} />
+        <ListNavigation title={CATEGORY.title} items={CATEGORY.items} />
+        <ListNavigation title={GENERAL.title} items={GENERAL.items} />
+      </div>
     </nav>
   );
 }
