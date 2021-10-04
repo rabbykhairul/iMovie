05b3310c16fb81f6be933f01999c9b7a2f358d7d
rabbykhairul <rabbykhairul@gmail.com>
2021-10-04 09:51:09 +0600

    render 'Header' inside 'LeftSideBar'

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./commons/Header";
 
 const LeftSideBar = () => {
   return (
     <nav className="left-side-bar">
-      
+      <Header />
     </nav>
   );
 }
