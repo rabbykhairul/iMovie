921b2ed4de6844d11cf1a747ea25f0c04afa000f
rabbykhairul <rabbykhairul@gmail.com>
2021-10-04 18:01:57 +0600

    import all the icons for 'LeftSideBar' navigation menu items

---
+++ src/utils/menuItems.js
@@ -0,0 +1,8 @@
+import { MdHomeFilled } from "react-icons/md";
+import { RiShieldCheckFill, RiSettings5Fill, RiCompass4Fill } from "react-icons/ri";
+import { AiFillCompass, AiFillClockCircle, AiFillStar } from "react-icons/ai";
+import { IoExit, IoVideocam } from "react-icons/io5";
+import { IoIosDownload, IoMdPerson } from "react-icons/io";
+import { TiThLarge } from "react-icons/ti";
+import { FaAward } from "react-icons/fa";
+
