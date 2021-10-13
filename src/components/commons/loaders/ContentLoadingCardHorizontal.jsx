8b0cc1729a14bfec734fa4c1a362de4654098881
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 12:12:02 +0600

    create 'ContentLoadingCardHorizontal' component to show loading state

---
+++ src/components/commons/loaders/ContentLoadingCardHorizontal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContentLoadingCardHorizontal = () => {
+  return (
+    <div className="content-loading-card-horizontal">
+      <div className="line line-sm"></div>
+      <div className="line line-lg"></div>
+      <div className="line line-md"></div>
+    </div>
+  );
+};
+
+export default ContentLoadingCardHorizontal;
