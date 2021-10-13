3ea8f123fce5f74c5eaf6492814610f2f60482f1
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 15:36:18 +0600

    WatchRecommendationList - display loader when items array is empty

--- src/components/commons/WatchRecommendationsList.jsx
+++ src/components/commons/WatchRecommendationsList.jsx
@@ -3,6 +3,7 @@ import Button from "./Button";
 
 import { BsThreeDots } from "react-icons/bs";
 import RecommendationCard from "./RecommendationCard";
+import ContentLoadingCardHorizontal from "./loaders/ContentLoadingCardHorizontal";
 
 const WatchRecommendationsList = (props) => {
   const { title = "Recommended", items = [] } = props;
@@ -17,7 +18,17 @@ const WatchRecommendationsList = (props) => {
   };
 
   const renderRecommendedItems = () => {
-    return items.map((item, idx) => <RecommendationCard key={idx} />);
+    if (items.length === 0)
+      return (
+        <>
+          <ContentLoadingCardHorizontal className="content-loading-card-small" />
+          <ContentLoadingCardHorizontal className="content-loading-card-small" />
+        </>
+      );
+
+    return items.map((item, idx) => (
+      <RecommendationCard key={idx} info={item} />
+    ));
   };
 
   const renderSeeMoreButton = () => <Button>See More</Button>;
