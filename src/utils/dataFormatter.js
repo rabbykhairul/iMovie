bd177c64af5bc5ffea2221525b6d78c14db9057f
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 23:21:43 +0600

    include actual genre names in tv/movie data after formatting

--- src/utils/dataFormatter.js
+++ src/utils/dataFormatter.js
@@ -1,17 +1,38 @@
-const formatGenresArray = (genres) => {
+import {
+  TV_GENRES,
+  MOVIE_GENRES,
+  TYPE_MOVIE,
+} from "../services/tvAndMovieService";
+
+const getGenreName = (genreId, type) => {
+  let genreName = "";
+  let genres = type === TYPE_MOVIE ? MOVIE_GENRES : TV_GENRES;
+
+  for (let genre of genres) {
+    if (genre.id === genreId) {
+      genreName = genre.name;
+      break;
+    }
+  }
+
+  return genreName;
+};
+
+const formatGenresArray = (genres, type) => {
   return genres.map((g) => {
     if (typeof g === "object") {
       return g.name;
-    } else return g;
+    } else return getGenreName(g, type);
   });
 };
 
-export const formatMovieTVDetails = (data) => {
+export const formatMovieTVDetails = (data, type) => {
   const formattedData = { ...data };
 
   formattedData.title = formattedData.title || formattedData.name;
   formattedData.genres = formatGenresArray(
-    formattedData.genres || formattedData.genre_ids
+    formattedData.genres || formattedData.genre_ids,
+    type
   );
   formattedData.rating = Math.ceil(formattedData.vote_average / 2);
 
