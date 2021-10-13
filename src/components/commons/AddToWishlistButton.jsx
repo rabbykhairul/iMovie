39d4770b8b476c71437fd4f81764ee7290b8fd40
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 12:09:38 +0600

    create 'AddToWishlistButton' component

---
+++ src/components/commons/AddToWishlistButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "./Button";
+
+const AddToWishlistButton = () => {
+  return <Button className="btn-add-to-wishlist">+</Button>;
+};
+
+export default AddToWishlistButton;
