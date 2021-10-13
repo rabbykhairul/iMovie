d739f96d53ef6f02d7ab4839770306c05239c245
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 20:43:13 +0600

    ContentSection - render Now Playing shows & on click update featured show

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -4,6 +4,8 @@ import MovieContext from "../contexts/MovieContext";
 import TVContext from "../contexts/TVContext";
 import TabList from "./commons/TabList";
 import FeaturedShowcard from "./commons/FeaturedShowCard";
+import Slider from "./commons/Slider";
+import InfoCardVertical from "./commons/InfoCardVertical";
 
 const TABS = [
   { title: "Movie", name: TYPE_MOVIE },
@@ -15,20 +17,56 @@ const ContentSection = () => {
   const tvInfo = useContext(TVContext);
 
   const [activeTab, setActiveTab] = useState(TABS[0]);
+  const [featuredMovie, setFeaturedMovie] = useState(null);
+  const [featuredTV, setFeaturedTV] = useState(null);
 
   const getFeaturedShow = () => {
-    if (activeTab.name === TYPE_MOVIE) return movieInfo.featuredShow;
-    else return tvInfo.featuredShow;
+    if (activeTab.name === TYPE_MOVIE)
+      return featuredMovie || movieInfo.featuredShow;
+    else return featuredTV || tvInfo.featuredShow;
+  };
+
+  const getNowPlayingTV = () => {
+    if (tvInfo.nowPlayingShows)
+      return tvInfo.nowPlayingShows.map((tvShow, idx) => (
+        <InfoCardVertical
+          key={idx}
+          info={tvShow}
+          onClick={() => setFeaturedTV(tvShow)}
+        />
+      ));
+    else return [];
+  };
+
+  const getNowPlayingMovies = () => {
+    if (movieInfo.nowPlayingShows)
+      return movieInfo.nowPlayingShows.map((movie, idx) => (
+        <InfoCardVertical
+          key={idx}
+          info={movie}
+          onClick={() => setFeaturedMovie(movie)}
+        />
+      ));
+    else return [];
+  };
+
+  const renderNowPlayingShows = () => {
+    if (activeTab.name === TYPE_MOVIE)
+      return <Slider title="Now Playing" items={getNowPlayingMovies()} />;
+    else return <Slider title="Now Playing" items={getNowPlayingTV()} />;
   };
 
   return (
     <div className="content-section">
-      <TabList
-        tabs={TABS}
-        activeTab={activeTab}
-        onClick={(tab) => setActiveTab(tab)}
-      />
-      <FeaturedShowcard featuredShow={getFeaturedShow()} />
+      <div className="inner-container" style={{ paddingBottom: "2vw" }}>
+        <TabList
+          tabs={TABS}
+          activeTab={activeTab}
+          onClick={(tab) => setActiveTab(tab)}
+        />
+        <FeaturedShowcard featuredShow={getFeaturedShow()} />
+        {renderNowPlayingShows()}
+      </div>
     </div>
   );
 };
