0f25fb186dbfffff640ed9fc4415eee5255b1acb
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 13:19:11 +0600

    consume TVContext & MovieContext inside ContentSection

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { TYPE_MOVIE, TYPE_TV } from "../services/tvAndMovieService";
+import MovieContext from "../contexts/MovieContext";
+import TVContext from "../contexts/TVContext";
 import TabList from "./commons/TabList";
 
 const TABS = [
@@ -8,6 +10,9 @@ const TABS = [
 ];
 
 const ContentSection = () => {
+  const movieInfo = useContext(MovieContext);
+  const tvInfo = useContext(TVContext);
+
   const [activeTab, setActiveTab] = useState(TABS[0]);
 
   return (
