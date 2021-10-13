142b3336b1390e11c70fc648237b6ae94b64bc09
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 15:37:59 +0600

    display popular movies & watchlists recommendations to the right side

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import TVContext from "../contexts/TVContext";
+import MovieContext from "../contexts/MovieContext";
 import SearchBar from "./commons/SearchBar";
+import WatchRecommendationsList from "./commons/WatchRecommendationsList";
 
 const RightSideBar = () => {
+  const tvInfo = useContext(TVContext);
+  const movieInfo = useContext(MovieContext);
+
+  const getPopularMoviesRecommendations = () =>
+    movieInfo.popularShows?.slice(0, 2);
+
+  const getWatchlistsRecommendations = () => tvInfo.popularShows?.slice(0, 4);
+
   return (
-    <aside className="right-side-bar" >
+    <aside className="right-side-bar">
       <SearchBar />
+      <div className="inner-container">
+        <WatchRecommendationsList
+          title="Popular Movies"
+          items={getPopularMoviesRecommendations()}
+        />
+        <WatchRecommendationsList
+          title="Watchlists"
+          items={getWatchlistsRecommendations()}
+        />
+      </div>
     </aside>
   );
 };
