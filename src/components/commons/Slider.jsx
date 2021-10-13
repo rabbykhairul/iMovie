ab7820d8bf27d4d98d6216d403e38ab9ffc247f1
rabbykhairul <rabbykhairul@gmail.com>
2021-10-13 19:54:16 +0600

    Slider - render items according to active slide index

--- src/components/commons/Slider.jsx
+++ src/components/commons/Slider.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentLoadingCardHorizontal from "./loaders/ContentLoadingCardHorizontal";
 
 const Slider = (props) => {
-  const { title = "Now Playing", items = [] } = props;
+  const { title = "Now Playing", items = [], itemsPerSlide = 4 } = props;
+
+  const [activeSlideIdx, setActiveSlideIdx] = useState(0);
 
   const renderSliderHeader = () => {
     return (
@@ -22,12 +24,24 @@ const Slider = (props) => {
     );
   };
 
+  const getItemsForCurrentSlide = () => {
+    const startIdx = activeSlideIdx * itemsPerSlide;
+    const endIdx = startIdx + itemsPerSlide;
+
+    return items.slice(startIdx, endIdx);
+  };
+
   const renderItems = () => {
     if (items.length === 0)
       return (
         <div className="slider-items-container">{renderLoadingContent()}</div>
       );
-    else return null;
+    else
+      return (
+        <div className="slider-items-container">
+          {getItemsForCurrentSlide()}
+        </div>
+      );
   };
 
   return (
